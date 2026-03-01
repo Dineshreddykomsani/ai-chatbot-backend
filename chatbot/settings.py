ac54bd67e005b6f48964012f4d5caa25490417b4
Dineshreddykomsani <dineshreddy4604@gmail.com>
2026-03-01 22:38:02 +0530

    Add WhiteNoise for production static handling

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -174,3 +175,5 @@ SWAGGER_SETTINGS = {
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 ALLOWED_HOSTS = ["*"]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
